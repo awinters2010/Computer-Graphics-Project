d0706093c6d8a4b5b31e36e5c7862bb978f4f4ac
Michael Yahner <mjy8@zips.uakron.edu>
2012-10-25 11:26:00 -0400

    Static class to manage the device

---
+++ Compiling/DeviceManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX.Direct3D9;
+
+namespace Graphics
+{
+    public static class DeviceManager
+    {
+        public static Device device;
+
+        public static void CreateDevice(IntPtr ptr, int width, int height)
+        {
+            device = new Device(new Direct3D(), 0, DeviceType.Hardware, ptr,
+                CreateFlags.HardwareVertexProcessing, new PresentParameters()
+                {
+                    BackBufferWidth = width,
+                    BackBufferHeight = height
+                });
+        }
+    }
+}
