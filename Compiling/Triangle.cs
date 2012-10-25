4192bc1473c9275947e9906b72b5cd19f2f7a0ed
Michael Yahner <mjy8@zips.uakron.edu>
2012-10-25 11:27:12 -0400

    rebuilt to drawn triangle in directx 9

--- Compiling/Triangle.cs
+++ Compiling/Triangle.cs
@@ -1,56 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SlimDX.Direct3D11;
 using SlimDX;
-using SlimDX.DXGI;
+using SlimDX.Direct3D9;
+using System.Drawing;
 
 namespace Graphics
 {
     public class Triangle : BasicShape
     {
+        Result r;
 
-        public Triangle(SlimDX.Direct3D11.Device device, DeviceContext context)
-            : base(device, context)
+        public Triangle(ref Device device)
+            : base(ref device)
+        {
+        }
+
+        public Triangle(ref Device device, ref Result r)
+            : base(ref device)
         {
             SetTriangle();
+            this.r = r;
         }
 
         private void SetTriangle()
         {
-            vertices = new DataStream(12 * 3, true, true);
-            vertices.Write(new Vector3(0.0f, 0.5f, 0.5f));
-            vertices.Write(new Vector3(0.5f, -0.5f, 0.5f));
-            vertices.Write(new Vector3(-0.5f, -0.5f, 0.5f));
-            vertices.Position = 0;
+            vertices = new VertexBuffer(device, 3 * 20, Usage.WriteOnly, VertexFormat.None, Pool.Default);
+            vertices.Lock(0, 0, LockFlags.None).WriteRange(new[] {
+                new VertexTransformed() { Color = Color.Red.ToArgb(), Position = new Vector4(200, 100.0f, 0.5f, 1.0f) },
+                new VertexTransformed() { Color = Color.Blue.ToArgb(), Position = new Vector4(400, 500.0f, 0.5f, 1.0f) },
+                new VertexTransformed() { Color = Color.Green.ToArgb(), Position = new Vector4(0, 500.0f, 0.5f, 1.0f) }
+            });
+            vertices.Unlock();
 
-            // create the vertex layout and buffer
-            var elements = new[] { new InputElement("POSITION", 0, Format.R32G32B32_Float, 0) };
-            var layout = new InputLayout(device, inputSignature, elements);
-            var vertexBuffer = new SlimDX.Direct3D11.Buffer(device, vertices, 12 * 3, ResourceUsage.Default, BindFlags.VertexBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+            var vertexElems = new[] {
+                        new VertexElement(0, 0, DeclarationType.Float4, DeclarationMethod.Default, DeclarationUsage.PositionTransformed, 0),
+                        new VertexElement(0, 16, DeclarationType.Color, DeclarationMethod.Default, DeclarationUsage.Color, 0),
+                                VertexElement.VertexDeclarationEnd
+                };
 
-            // configure the Input Assembler portion of the pipeline with the vertex data
-            context.InputAssembler.InputLayout = layout;
-            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
-            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertexBuffer, 12, 0));
+            var vertexDecl = new VertexDeclaration(device, vertexElems);
 
-            // set the shaders
-            context.VertexShader.Set(vertexShader);
-            context.PixelShader.Set(pixelShader);
+            r = device.SetStreamSource(0, vertices, 0, 20);
+            device.VertexDeclaration = vertexDecl;
+
+            BasicShape.VerticesCount += 3;
         }
 
         public override void Render()
         {
-            context.Draw(3, 0);
-        }
-
-        public void Destroy()
-        {
-            vertices.Close();
-            inputSignature.Dispose();
-            vertexShader.Dispose();
-            pixelShader.Dispose();
+            r = device.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
         }
     }
 }
