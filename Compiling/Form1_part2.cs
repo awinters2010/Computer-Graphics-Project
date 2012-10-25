d0e9473ef7ef1acec71783a737ede0dde7a7b49e
Michael Yahner <mjy8@zips.uakron.edu>
2012-10-25 11:29:20 -0400

    cleaned up
    removed device creation from here and put in its new static class
    DeviceManager. There will only ever be one instance of the device so
    made a class for it and made it static. also added another thread start
    for displaying some potentially relevant information

--- Compiling/Form1_part2.cs
+++ Compiling/Form1_part2.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SlimDX.DXGI;
-using SlimDX;
-using SlimDX.Direct3D11;
+﻿using System.Collections.Generic;
 using System.Threading;
+using SlimDX.Direct3D9;
+using System.Drawing;
 
 namespace Graphics
 {
@@ -13,56 +9,40 @@ namespace Graphics
     {
         List<BasicShape> renderable = new List<BasicShape>();
 
-        public void createDeviceAndSwapChain(System.Windows.Forms.Control form)
-        {
-            var description = new SwapChainDescription()
-            {
-                BufferCount = 1,
-                Usage = Usage.RenderTargetOutput,
-                OutputHandle = panel1.Handle,
-                IsWindowed = true,
-                ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.R8G8B8A8_UNorm),
-                SampleDescription = new SampleDescription(1, 0),
-                Flags = SwapChainFlags.AllowModeSwitch,
-                SwapEffect = SwapEffect.Discard
-            };
-            SlimDX.Direct3D11.Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.None, description, out device, out swapChain);
-
-            // create a view of our render target, which is the backbuffer of the swap chain we just created
-            using (var resource = SlimDX.Direct3D11.Resource.FromSwapChain<SlimDX.Direct3D11.Texture2D>(swapChain, 0))
-                renderTarget = new RenderTargetView(device, resource);
-
-            // setting a viewport is required if you want to actually see anything
-            context = device.ImmediateContext;
-            viewport = new SlimDX.Direct3D11.Viewport(0.0f, 0.0f, panel1.ClientSize.Width,
-                panel1.ClientSize.Height);
-            context.OutputMerger.SetTargets(renderTarget);
-            context.Rasterizer.SetViewports(viewport);
-
-            // prevent DXGI handling of alt+enter, which doesn't work properly with Winforms
-            using (var factory = swapChain.GetParent<Factory>())
-                factory.SetWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAltEnter);
-
-            // handle alt+enter ourselves
-            form.KeyDown += (o, e) =>
-            {
-                if (e.Alt && e.KeyCode == System.Windows.Forms.Keys.Enter)
-                {
-                    swapChain.IsFullScreen = !swapChain.IsFullScreen;
-                }
-            };
-        }
+        /// <summary>
+        /// Delegate to update things
+        /// </summary>
+        /// <param name="text">the upated text</param>
+        private delegate void update(string text);
 
         public void renderScene()
         {
             while (true)
             {
-                context.ClearRenderTargetView(renderTarget, new Color4(0.25f, 0.75f, 0.25f));
-                foreach (var item in renderable)
+                DeviceManager.device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+                DeviceManager.device.BeginScene();
+                lock (renderable)
                 {
-                    item.Render();
+                    foreach (var item in renderable)
+                    {
+                        item.Render();
+                    }
                 }
-                swapChain.Present(0, PresentFlags.None);
+                DeviceManager.device.EndScene();
+                DeviceManager.device.Present();
+
+                System.Diagnostics.Debug.WriteLine(renderable.Count);
+            }
+        }
+
+        private void UpdateText()
+        {
+            while (true)
+            {
+                lblMemoryUsage.Invoke(new update(this.LblUpdate),
+                    new object[] { (System.Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64 / 1048576).ToString() });
+                lblMem.Invoke(new update(this.OLblUpdate),
+                    new object[] { "Vertices: " + BasicShape.VerticesCount.ToString() });
             }
         }
 
@@ -70,6 +50,9 @@ namespace Graphics
         {
             renderThread = new Thread(new ThreadStart(renderScene));
             renderThread.Start();
+
+            status = new Thread(new ThreadStart(UpdateText));
+            status.Start();
         }
     }
 }
