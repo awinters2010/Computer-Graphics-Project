44026e8bbb334a463c58e2d1f8fbb9ce226ca7ff
Michael Yahner <mjy8@zips.uakron.edu>
2012-10-25 11:30:25 -0400

    added more
    now sets 3D9 view creation and projection setup

--- Compiling/Camera.cs
+++ Compiling/Camera.cs
@@ -1,22 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SlimDX;
+﻿using SlimDX;
+using SlimDX.Direct3D9;
+using System;
 
 namespace Graphics
 {
     public class Camera
     {
-        public Vector3 position;
+        // where we are stationed at
+        public Vector3 eye;
+        // what we are looking at (location)
         public Vector3 lookAt;
+        // which way is up
         public Vector3 up;
+        // how far away are we from origin
+        public float distanceFromCamera = 5f;
+        // the actual view from eye, lookat, and up
+        public Matrix view;
+        
+        // field of view how wide can we see
+        public float fov;
+        // how the screen is displayed such as wide screen
+        public float aspectRatio;
+        // how close to us ( camera ) are things drawn
+        public float near;
+        // how far away from us are things drawn
+        public float far;
+        // project ( what is actually being seen ) from fov, aspect, near, and far
+        public Matrix projection;
 
+        /// <summary>
+        /// Constructor that sets basic values for view and projection
+        /// </summary>
         public Camera()
         {
-            position = new Vector3(0, 0, 50f);
+            eye = new Vector3(0, 0, distanceFromCamera);
             lookAt = Vector3.Zero;
-            up = new Vector3(0, 1, 0);
+            up = Vector3.UnitY;
+
+            view = Matrix.LookAtLH(eye, lookAt, up);
+            DeviceManager.device.SetTransform(TransformState.View, view);
+
+            fov = (float)Math.PI / 4.0f;
+            aspectRatio = (float)DeviceManager.device.Viewport.Width / DeviceManager.device.Viewport.Height;
+            near = 1.0f;
+            far = 100.0f;
+
+            projection = Matrix.PerspectiveFovLH(fov, aspectRatio,
+                near, far);
+            DeviceManager.device.SetTransform(TransformState.Projection,
+                projection);
+        }
+        
+
+        /// <summary>
+        /// Sets a new view
+        /// </summary>
+        /// <param name="eye"> where are we</param>
+        /// <param name="lookat"> what are we looking at</param>
+        /// <param name="up"> which way is up</param>
+        public void SetView(Vector3 eye, Vector3 lookat, Vector3 up)
+        {
+            this.eye = eye;
+            this.lookAt = lookat;
+            this.up = up;
+            view = Matrix.LookAtLH(this.eye, lookAt, this.up);
+            DeviceManager.device.SetTransform(TransformState.View, view);
+        }
+
+        /// <summary>
+        /// Sets a new projection
+        /// </summary>
+        /// <param name="fov"> how wide do we see (in radians)</param>
+        /// <param name="aspectRatio"> width/height</param>
+        /// <param name="close"> how close do we draw objects</param>
+        /// <param name="far"> how far away do we draw objects</param>
+        public void SetProjection(float fov, float aspectRatio, 
+            float close, float far)
+        {
+            this.fov = fov;
+            this.aspectRatio = aspectRatio;
+            near = close;
+            this.far = far;
+            projection = Matrix.PerspectiveFovLH(this.fov, this.aspectRatio,
+                near, this.far);
+            DeviceManager.device.SetTransform(TransformState.Projection,
+                projection);
+        }
+
+        public void ChangeView(Vector3 eye, Vector3 lookAt)
+        {
+            this.eye = eye;
+            this.lookAt = lookAt;
+            view = Matrix.LookAtLH(this.eye, this.lookAt, up);
+            DeviceManager.device.SetTransform(TransformState.View, view);
         }
     }
 }
