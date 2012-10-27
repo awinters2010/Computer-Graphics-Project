3f81c23aa9c63cb65227b5a88069fd7cb473afd4
Michael Yahner <mjy8@zips.uakron.edu>
2012-10-27 13:32:11 -0400

    started input class

---
+++ Compiling/Input.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX.DirectInput;
+
+namespace Graphics
+{
+    public static class Input
+    {
+        DirectInput input;
+        static Keyboard keyBoard;
+        Mouse mouse;
+
+        public Input()
+        {
+            input = new DirectInput();
+            keyBoard = new Keyboard(input);
+        }
+
+        public static bool KeyPress()
+        {
+            if (keyBoard.)
+            {
+                
+            }
+        }
+    }
+}
