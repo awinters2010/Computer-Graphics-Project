1b31182e3735c2edb735aedc4befd5b6fdfef523
Michael Yahner <mjy8@zips.uakron.edu>
2012-10-27 11:41:16 -0400

    added new vertex untransformed draw code

--- Compiling/Triangle.cs
+++ Compiling/Triangle.cs
@@ -9,6 +9,7 @@ namespace Graphics
     public class Triangle : BasicShape
     {
         Result r;
+        private const int triangleVerticies = 3;
 
         public Triangle(ref Device device)
             : base(ref device)
@@ -24,6 +25,7 @@ namespace Graphics
 
         private void SetTriangle()
         {
+            /*
             vertices = new VertexBuffer(device, 3 * 20, Usage.WriteOnly, VertexFormat.None, Pool.Default);
             vertices.Lock(0, 0, LockFlags.None).WriteRange(new[] {
                 new VertexTransformed() { Color = Color.Red.ToArgb(), Position = new Vector4(200, 100.0f, 0.5f, 1.0f) },
@@ -42,8 +44,31 @@ namespace Graphics
 
             r = device.SetStreamSource(0, vertices, 0, 20);
             device.VertexDeclaration = vertexDecl;
+             * */
 
-            BasicShape.VerticesCount += 3;
+            VertexUntransformed[] vertex = 
+            {
+                new VertexUntransformed() { Position = new Vector3(0f, 1, 5), Color = Color.Orange.ToArgb() },
+                new VertexUntransformed() { Position = new Vector3(1, -1, 5), Color = Color.Purple.ToArgb() },
+                new VertexUntransformed() { Position = new Vector3(-1, -1, 5),Color = Color.Gray.ToArgb() },
+            };
+
+            vertices = new VertexBuffer(device, triangleVerticies * VertexUntransformed.VertexByteSize, Usage.WriteOnly, VertexFormat.None, Pool.Default);
+            vertices.Lock(0, 0, LockFlags.None).WriteRange(vertex);
+            vertices.Unlock();
+
+            var vertexElems = new[] {
+                        new VertexElement(0, 0, DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.Position, 0),
+                        new VertexElement(0, 12, DeclarationType.Color, DeclarationMethod.Default, DeclarationUsage.Color, 0),
+                                VertexElement.VertexDeclarationEnd
+                };
+
+            BasicShape.VerticesCount += triangleVerticies;
+
+            var vertexDecl = new VertexDeclaration(device, vertexElems);
+
+            r = device.SetStreamSource(0, vertices, 0, VertexUntransformed.VertexByteSize);
+            device.VertexDeclaration = vertexDecl;
         }
 
         public override void Render()
