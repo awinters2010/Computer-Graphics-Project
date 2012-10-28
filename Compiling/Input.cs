dd7e1ced4c6cf5b423070b2ae160d5d623c66680
Michael Yahner <mjy8@zips.uakron.edu>
2012-10-28 19:23:22 -0400

    needs lots of work
    removed everything because it just wasnt working
    will need this class for when we have the "live" mode for actually
    "playing" the creation (only because this class will not have the lag
    that regular win form windows do)

--- Compiling/Input.cs
+++ Compiling/Input.cs
@@ -3,22 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SlimDX.DirectInput;
+using SlimDX;
+using System.Windows.Forms;
 
 namespace Graphics
 {
-    public static class Input
+    public class Input
     {
-        static DirectInput input;
-        static Keyboard keyBoard;
-        static Mouse mouse;
-
-        public static void Initialize()
-        {
-            input = new DirectInput();
-            keyBoard = new Keyboard(input);
-            keyBoard.Acquire();
-            mouse = new Mouse(input);
-            mouse.Acquire();
-        }
     }
 }
