38891905c581ca84fb297eeb013fe1a45604b0ff
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-02 21:36:49 -0400

    added this for making 3d stuff
    in its own class because it will become bigger as we add stuff for
    textures and lighting

---
+++ Compiling/VertexUntransformed.cs
@@ -0,0 +1,21 @@
+﻿using SlimDX;
+using SlimDX.Direct3D9;
+
+namespace Graphics
+{
+    struct VertexUntransformed
+    {
+        public Vector3 Position { get; set; }
+        public int Color { get; set; }
+
+        public static VertexFormat format = VertexFormat.Position | VertexFormat.Diffuse;
+        public const int VertexByteSize = 16;
+
+        public static VertexDeclaration vertexDecl = new VertexDeclaration(DeviceManager.device, new VertexElement[]
+        {
+            new VertexElement(0, 0, DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.Position, 0),
+            new VertexElement(0, 12, DeclarationType.Color, DeclarationMethod.Default, DeclarationUsage.Color, 0),
+            VertexElement.VertexDeclarationEnd
+        });
+    }
+}
