6c808d9612da6ca33a61ba4a28d04359858ec97a
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-13 11:53:21 -0500

    added vertices and indicies

--- Compiling/Triangle.cs
+++ Compiling/Triangle.cs
@@ -13,10 +13,27 @@ namespace Graphics
         public Matrix World { get; private set; }
         public short[] ShapeIndices { get; private set; }
         public VertexUntransformed[] ShapeVertices { get; private set; }
+        public string Name { get; set; }
 
         public Triangle()
-        { 
-            
+        {
+            ShapeVertices = new VertexUntransformed[] {
+                    new VertexUntransformed() { Color = Color.Red.ToArgb(), Position = new Vector3(-1f, 0f, 1f) },
+                    new VertexUntransformed() { Color = Color.Blue.ToArgb(), Position = new Vector3(1f, 0f, 1f) },
+                    new VertexUntransformed() { Color = Color.Blue.ToArgb(), Position = new Vector3(-1f, 0f, -1f) },
+                    new VertexUntransformed() { Color = Color.Red.ToArgb(), Position = new Vector3(1f, 0f, -1f) },
+
+                    new VertexUntransformed() { Color = Color.Orange.ToArgb(), Position = new Vector3(0f, 1f, 0f) },
+                };
+
+            ShapeIndices = new short[] {
+                0, 2, 1,    // base
+                1, 2, 3,
+                0, 1, 4,    // sides
+                1, 3, 4,
+                3, 2, 4,
+                2, 0, 4,
+            };
         }
 
         public void Rotate()
@@ -29,7 +46,7 @@ namespace Graphics
             throw new NotImplementedException();
         }
 
-        public void Scale() 
+        public void Scale()
         {
             throw new NotImplementedException();
         }
