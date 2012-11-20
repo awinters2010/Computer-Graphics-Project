61b1c469b03b2df636bc60105ff3e031863a1d78
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-20 12:51:55 -0500

    added summaries
    added scale and rotation field
    also added parameters to rotation and scale

--- Compiling/IShape.cs
+++ Compiling/IShape.cs
@@ -11,10 +11,34 @@ namespace Graphics
         short[] ShapeIndices { get; }
         string Name { get; set; }
         string Type { get; }
+        Vector3 Scaling { get; set; }
+        Vector3 Rotation { get; set; }
 
-        void Rotate();
+        /// <summary>
+        /// Rotate the object
+        /// </summary>
+        /// <param name="x">x unit to rotate</param>
+        /// <param name="y">y unit to rotate</param>
+        /// <param name="z">z unit to rotate</param>
+        void Rotate(float x, float y, float z);
+
+        /// <summary>
+        /// Move the object
+        /// </summary>
+        /// <param name="x">x unit amount to move</param>
+        /// <param name="y">y unit amount to move</param>
+        /// <param name="z">z unit amount to move</param>
         void Translate(float x, float y, float z);
-        void Scale();
+
+        /// <summary>
+        /// Scale the object (change size)
+        /// </summary>
+        /// <param name="scale">for the three axis that can be scaled</param>
+        void Scale(Vector3 scale);
+
+        /// <summary>
+        /// Set the translation, scale, and rotation for the device for the object
+        /// </summary>
         void Render();
     }
 }
