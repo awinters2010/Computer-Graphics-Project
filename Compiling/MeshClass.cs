a6ff1df3b69b44ae0a64c976868e78370eca29c3
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-21 20:43:13 -0500

    finished setting up the mesh class
    can draw a mesh but now primitives can't be drawn at the same time. will
    fix that later

--- Compiling/MeshClass.cs
+++ Compiling/MeshClass.cs
@@ -1,21 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using SlimDX.Direct3D9;
 
 namespace Graphics
 {
-    public class MeshClass
+    public class MeshClass: IDisposable
     {
-        Material[] m;
-        Texture[] t;
-        Mesh mesh;
+        public ExtendedMaterial[] m;
+        public Texture[] t;
+        public Material[] mat;
+        public Mesh mesh;
         uint nummaterials = 0;
 
-        public MeshClass(string file)
+        public MeshClass(string file, string fileName)
         {
-            mesh = Mesh.FromFile(DeviceManager.LocalDevice, file, MeshFlags.UseHardwareOnly);
+            mesh = Mesh.FromFile(DeviceManager.LocalDevice, file, MeshFlags.SystemMemory);
+            m = mesh.GetMaterials();
+            mat = new Material[m.Length];
+            t = new Texture[m.Length];
+            for (int i = 0; i < m.Length; i++)
+            {
+                mat[i] = m[i].MaterialD3D;
+                mat[i].Ambient = mat[i].Diffuse;
+                string s = file;
+                Console.WriteLine(s.Length);
+                int index = s.IndexOf(fileName);
+                string tex = s.Remove(index);
+                s = tex.Insert(index, m[i].TextureFileName);
+                Console.WriteLine(s);
+                Console.WriteLine(tex);
+                
+                t[i] = Texture.FromFile(DeviceManager.LocalDevice, s);
+            }
+        }
+
+        ~MeshClass()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i].Dispose();
+            }
+
+            mesh.Dispose();
+        }
+
+        public void RenderMesh()
+        {
+            for (int i = 0; i < mat.Length; ++i)
+            {
+                DeviceManager.LocalDevice.Material = mat[i];
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                DeviceManager.LocalDevice.SetTexture(0, t[i]);
+            }
+
+            mesh.DrawSubset(0);
         }
     }
 }
