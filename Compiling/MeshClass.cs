7bc540131b47bbe2e4efeff004313fd538e0116c
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-24 13:51:23 -0500

    added methods
    methods for rotation, scaling, translation, adding, color, adding
    textures

--- Compiling/MeshClass.cs
+++ Compiling/MeshClass.cs
@@ -8,22 +8,24 @@ namespace Graphics
     public class MeshClass : IDisposable
     {
         public Texture[] CurrentTexture { get; set; }
-        private Material[] material;
-        public Mesh mesh;
-        public Vector3 Position { get; set; }
+        public Vector3 ObjectPosition { get; set; }
         public Matrix World { get; set; }
-        public Vector3 Roate { get; set; }
+        public Vector3 ObjectRotate { get; set; }
         public string Type { get; private set; }
         public string Name { get; set; }
+        public Vector3 ObjectScale { get; set; }
+
+        private Material[] material;
+        private Mesh mesh;
 
         /// <summary>
-        /// 
+        /// Create a mesh from a .x File
         /// </summary>
-        /// <param name="file"></param>
-        /// <param name="fileName"></param>
-        public MeshClass(string file, string fileName)
+        /// <param name="filePath">The path of the file</param>
+        /// <param name="fileName">The name of the file</param>
+        public MeshClass(string filePath, string fileName)
         {
-            mesh = Mesh.FromFile(DeviceManager.LocalDevice, file, MeshFlags.SystemMemory);
+            mesh = Mesh.FromFile(DeviceManager.LocalDevice, filePath, MeshFlags.SystemMemory);
             ExtendedMaterial[] m = mesh.GetMaterials();
             material = new Material[m.Length];
             CurrentTexture = new Texture[m.Length];
@@ -33,7 +35,7 @@ namespace Graphics
                 material[i] = m[i].MaterialD3D;
                 material[i].Ambient = material[i].Diffuse;
 
-                string s = file;
+                string s = filePath;
                 int index = s.IndexOf(fileName);
                 string tex = s.Remove(index);
                 s = tex.Insert(index, m[i].TextureFileName);
@@ -41,15 +43,16 @@ namespace Graphics
                 CurrentTexture[i] = Texture.FromFile(DeviceManager.LocalDevice, s);
             }
 
-            Position = Vector3.Zero;
+            ObjectPosition = Vector3.Zero;
+            ObjectRotate = Vector3.Zero;
             World = Matrix.Identity;
             Name = fileName;
         }
 
         /// <summary>
-        /// 
+        /// For creating shape objects
         /// </summary>
-        /// <param name="type"></param>
+        /// <param name="type">the name of the object you wish to create</param>
         public MeshClass(string type)
         {
             if (type == "cube")
@@ -83,14 +86,15 @@ namespace Graphics
 
                 mesh.LockIndexBuffer(LockFlags.None).WriteRange<short>(ShapeIndices);
                 mesh.UnlockIndexBuffer();
-
-                //mesh = new Mesh(DeviceManager.LocalDevice, faceCount, vertexCount, options, vDec);
             }
 
-            Position = Vector3.Zero;
-            World = Matrix.Translation(Position);
+            ObjectPosition = Vector3.Zero;
+            ObjectRotate = Vector3.Zero;
+            World = Matrix.Translation(ObjectPosition);
             Name = type;
         }
+
+        #region releasing resources
 
         ~MeshClass()
         {
@@ -110,9 +114,11 @@ namespace Graphics
             mesh.Dispose();
         }
 
+        #endregion
+
         public void RenderMesh()
         {
-            World = Matrix.Translation(Position);
+            World = Matrix.Translation(ObjectPosition);
             DeviceManager.LocalDevice.SetTransform(TransformState.World, World);
 
             if (material != null)
@@ -134,6 +140,105 @@ namespace Graphics
             mesh.DrawSubset(0);
         }
 
+        /// <summary>
+        /// To apply Color to the object
+        /// </summary>
+        /// <param name="ambient">The ambient color (color of the whole object)</param>
+        public void ColorMesh(Color ambient)
+        {
+            this.material[0].Ambient = ambient;
+            this.material[0].Diffuse = Color.White;
+            this.material[0].Specular = Color.White;
+            this.material[0].Emissive = Color.White;
+            this.material[0].Power = 1.0f;
+        }
 
+        /// <summary>
+        /// To apply Color to the object
+        /// </summary>
+        /// <param name="ambient">The ambient color (color of the whole object)</param>
+        /// <param name="diffuse"></param>
+        public void ColorMesh(Color ambient, Color diffuse)
+        {
+            this.material[0].Ambient = ambient;
+            this.material[0].Diffuse = diffuse;
+            this.material[0].Specular = Color.White;
+            this.material[0].Emissive = Color.White;
+            this.material[0].Power = 1.0f;
+        }
+
+        /// <summary>
+        /// To apply Color to the object
+        /// </summary>
+        /// <param name="ambient">The ambient color (color of the whole object)</param>
+        /// <param name="diffuse"></param>
+        /// <param name="specular"></param>
+        public void ColorMesh(Color ambient, Color diffuse, Color specular)
+        {
+            this.material[0].Ambient = ambient;
+            this.material[0].Diffuse = diffuse;
+            this.material[0].Specular = specular;
+            this.material[0].Emissive = Color.White;
+            this.material[0].Power = 1.0f;
+        }
+
+        /// <summary>
+        /// To apply Color to the object
+        /// </summary>
+        /// <param name="ambient">The ambient color (color of the whole object)</param>
+        /// <param name="diffuse"></param>
+        /// <param name="specular"></param>
+        /// <param name="emissive"></param>
+        public void ColorMesh(Color ambient, Color diffuse, Color specular, Color emissive)
+        {
+            this.material[0].Ambient = ambient;
+            this.material[0].Diffuse = diffuse;
+            this.material[0].Specular = specular;
+            this.material[0].Emissive = emissive;
+            this.material[0].Power = 1.0f;
+        }
+
+        /// <summary>
+        /// To apply Color to the object
+        /// </summary>
+        /// <param name="ambient">The ambient color (color of the whole object)</param>
+        /// <param name="diffuse"></param>
+        /// <param name="specular"></param>
+        /// <param name="emissive"></param>
+        /// <param name="power"></param>
+        public void ColorMesh(Color ambient, Color diffuse, Color specular, Color emissive, float power)
+        {
+            this.material[0].Ambient = ambient;
+            this.material[0].Diffuse = diffuse;
+            this.material[0].Specular = specular;
+            this.material[0].Emissive = emissive;
+            this.material[0].Power = power;
+        }
+
+        /// <summary>
+        /// To apply a user supplied texture to the object
+        /// </summary>
+        /// <param name="filePath">The string path of the file</param>
+        /// <param name="fileName">The string name of the file</param>
+        public void ApplyTextureMesh(string filePath, string fileName)
+        {
+            this.CurrentTexture[0] = Texture.FromFile(DeviceManager.LocalDevice, filePath);
+        }
+
+
+        public void Rotate(float x, float y, float z)
+        {
+            ObjectRotate = new Vector3(x, y, z);
+        }
+
+        public void Translate(float x, float y, float z)
+        {
+            ObjectPosition = new Vector3(x, y, z);
+        }
+
+        public void Scale(Vector3 scale)
+        {
+            ObjectScale = scale;
+        }
     }
 }
