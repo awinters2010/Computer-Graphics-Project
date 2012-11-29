e691b28d1e1d0699d2e48318aa0588787a00a8e5
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-29 12:49:18 -0500

    Added a class declaration
    Renamed vertexuntransformed to vertexpositioncolor
    added another structure - vertexpositionnormalcolor

---
+++ Compiling/CustomVertex.cs
@@ -0,0 +1,42 @@
+﻿using SlimDX;
+using SlimDX.Direct3D9;
+
+namespace Graphics
+{
+    public class CustomVertex
+    {
+        public struct VertexPositionColor
+        {
+            public Vector3 Position { get; set; }
+            public int Color { get; set; }
+
+            public static VertexFormat Format = VertexFormat.Position | VertexFormat.Diffuse;
+            public static int VertexByteSize = 16;
+
+            public static VertexDeclaration VertexDecl = new VertexDeclaration(DeviceManager.LocalDevice, new VertexElement[]
+            {
+                new VertexElement(0, 0, DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.Position, 0),
+                new VertexElement(0, 12, DeclarationType.Color, DeclarationMethod.Default, DeclarationUsage.Color, 0),
+                VertexElement.VertexDeclarationEnd
+            });
+        }
+
+        public struct VertexPositionNormalColor
+        {
+            public Vector3 Position { get; set; }
+            public Vector3 Normals { get; set; }
+            public int Color { get; set; }
+
+            public static VertexFormat Format = VertexFormat.PositionNormal | VertexFormat.Diffuse;
+            public static int VertexByteSize = 28;
+
+            public static VertexDeclaration VertexDecl = new VertexDeclaration(DeviceManager.LocalDevice, new VertexElement[]
+            {
+                new VertexElement(0, 0, DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.Position, 0),
+                new VertexElement(0, 12, DeclarationType.Float3, DeclarationMethod.Default, DeclarationUsage.Normal, 0),
+                new VertexElement(0, 24, DeclarationType.Color, DeclarationMethod.Default, DeclarationUsage.Color, 0),
+                VertexElement.VertexDeclarationEnd
+            });
+        }
+    }
+}
