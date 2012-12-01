ac225696a43c6e3abb6443b52d80776dd361086e
Michael Yahner <mjy8@zips.uakron.edu>
2012-12-01 12:16:30 -0500

    added a presentparameters variable incase device
    lost
    made reset method

--- Compiling/DeviceManager.cs
+++ Compiling/DeviceManager.cs
@@ -10,19 +10,43 @@ namespace Graphics
     {
         public static Device LocalDevice { get; set; }
 
+        private static PresentParameters presentationParameters;
+
+        public static PresentParameters PresentationParameters
+        {
+            get
+            {
+                return presentationParameters;
+            }
+        }
+
         public static void CreateDevice(IntPtr ptr, int width, int height)
         {
+            presentationParameters = new PresentParameters()
+            {
+                BackBufferWidth = width,
+                BackBufferHeight = height,
+                BackBufferFormat = Format.A8R8G8B8,
+                BackBufferCount = 1,
+                Multisample = MultisampleType.None,
+                MultisampleQuality = 0,
+                SwapEffect = SwapEffect.Discard,
+                Windowed = true,
+                DeviceWindowHandle = ptr,
+                EnableAutoDepthStencil = true,
+                AutoDepthStencilFormat = Format.D16,
+                PresentFlags = PresentFlags.None,
+                FullScreenRefreshRateInHertz = 0,
+                PresentationInterval = PresentInterval.Default
+            };
+
             LocalDevice = new Device(new Direct3D(), 0, DeviceType.Hardware, ptr,
-                CreateFlags.HardwareVertexProcessing, new PresentParameters()
-                {
-                    BackBufferWidth = width,
-                    BackBufferHeight = height,
-                    Windowed = true,
-                    SwapEffect = SwapEffect.Discard,
-                    BackBufferCount = 1,
-                    EnableAutoDepthStencil = true,
-                    AutoDepthStencilFormat = Format.D16,
-                });
+                CreateFlags.HardwareVertexProcessing, presentationParameters);
+        }
+
+        public static void ResetDevice()
+        {
+            LocalDevice.Reset(presentationParameters);
         }
     }
 }
