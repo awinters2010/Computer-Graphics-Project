db8bb18cd9acd65a0cd82b9b4f6ad997a0dd8be8
Michael Yahner <mjy8@zips.uakron.edu>
2012-12-01 12:15:52 -0500

    renamed light to Lights
    removed vertices and indices count
    added a method for checking to see if the device is lost - not finished
    ( will check correctly but wont reset device correctly - i think)

--- Compiling/Renderer.cs
+++ Compiling/Renderer.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.ObjectModel;
-using SlimDX.Direct3D9;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Collections.Generic;
-using System;
+using System.Windows.Forms;
+using SlimDX;
+using SlimDX.Direct3D9;
 
 namespace Graphics
 {
@@ -11,18 +11,16 @@ namespace Graphics
         private volatile bool shutdown;
 
         public Terrain Terrian { get; set; }
-        public int verticesCount { get; set; }
-        public int indiciesCount { get; set; }
         public List<MeshClass> Meshes { get; set; }
-        public List<Lights> light { get; set; }
+        public List<LightClass> Lights { get; set; }
 
         public Renderer()
         {
             shutdown = false;
             Meshes = new List<MeshClass>();
-            light = new List<Lights>();
-            light.Add(new Lights());
-            light[0].LightOnOff(0);
+            Lights = new List<LightClass>();
+            Lights.Add(new LightClass());
+            Lights[0].LightOnOff(0);
         }
 
         public void RequestShutdown()
@@ -34,33 +32,65 @@ namespace Graphics
         {
             while (!shutdown)
             {
-                DeviceManager.LocalDevice.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
-                DeviceManager.LocalDevice.BeginScene();
-
-                lock (Meshes)
+                if (!IsDeviceLost())
                 {
-                    foreach (var item in Meshes)
+                    DeviceManager.LocalDevice.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+                    DeviceManager.LocalDevice.BeginScene();
+
+                    lock (Meshes)
                     {
-                        item.RenderMesh();
+                        foreach (var item in Meshes)
+                        {
+                            item.RenderMesh();
+                        }
                     }
-                }
 
-                lock (light)
-                {
-                    foreach (var item in light)
+                    lock (Lights)
                     {
-                        item.Render();
+                        foreach (var item in Lights)
+                        {
+                            item.Render();
+                        }
                     }
-                }
 
-                if (Terrian != null)
-                {
-                    Terrian.Render();
-                }
+                    if (Terrian != null)
+                    {
+                        Terrian.Render();
+                    }
 
-                DeviceManager.LocalDevice.EndScene();
-                DeviceManager.LocalDevice.Present();
+                    DeviceManager.LocalDevice.EndScene();
+                    DeviceManager.LocalDevice.Present();
+                }
             }
+        }
+
+        private bool IsDeviceLost()
+        {
+            Result result = DeviceManager.LocalDevice.TestCooperativeLevel();
+
+            if (result == ResultCode.DeviceLost)
+            {
+                return true;
+            }
+
+            else if (result == ResultCode.DriverInternalError)
+            {
+                string s = "Error Code: " + result.Code + "\n";
+                s += "Error Data: " + result.Data + "\n";
+                s += "Error Description: " + result.Description + "\n";
+
+                System.IO.File.WriteAllText("error.txt", s);
+                MessageBox.Show("Interal Driver ERROR!!");
+                Application.Exit();
+                return true;
+            }
+            else if (result == ResultCode.DeviceNotReset)
+            {
+                DeviceManager.ResetDevice();
+                return false;
+            }
+
+            return false;
         }
     }
 }
